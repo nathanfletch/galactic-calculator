9f1e06556b8939b75567b9db986a40c16862e052
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-03 09:36:23 -0700

    Add failing test for getAge()

--- __tests__/planet.test.js
+++ __tests__/planet.test.js
@@ -1,9 +1,17 @@
 import Planet from "./../src/planet";
 
 describe("Planet", () => {
+  let myPlanet;
+  beforeEach(() => {
+    myPlanet = new Planet(38, 88);
+  })
   test("should create a planet object with the input age and number of days in the solar year for the planet", () => {
-    const myPlanet = new Planet(38, 88);
     expect(myPlanet.earthAgeOfUser).toEqual(38);
     expect(myPlanet.daysInSolarYear).toEqual(88);
   });
+  
+  test("should return the user's age on the planet", () => {
+    expect(myPlanet.getAge()).toEqual(157);
+    
+  })
 });
--- src/planet.js
+++ src/planet.js
@@ -12,9 +12,9 @@ export default class Planet {
     */
   }
   
-  // getAge() {
-
-  // }
+  getAge() {
+    
+  }
   /*
   getYearsLeft(country, activityLevel)
   getTravelTime
