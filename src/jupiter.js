7e2df2a30af896e168f8d36e32317c8b03068e7f
Nathan Fletcher <nathan.fletcher@gmail.com>
2021-09-03 13:02:51 -0700

    Add Jupiter constructor (passing)

--- src/jupiter.js
+++ src/jupiter.js
@@ -2,7 +2,6 @@ import Planet from './planet';
 
 export default class Jupiter extends Planet {
   constructor(age) {
-    super();
-    //age, "Jupiter", 4,333, 376
+    super(age, "Jupiter", 4333, 376);
   }
 }
